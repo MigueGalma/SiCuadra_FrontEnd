56a1afb0ce839a5c55f0757a883a2f0267a84b13
MigueGalma <miguelignaciogalmarini@outlook.com>
2023-12-18 14:43:26 -0300

    terminado de front end

--- js/import.js
+++ js/import.js
@@ -1,10 +1,43 @@
-const url = 'http://127.0.0.1:5000/respuesta/answer.json';
+// Captura del JSON
 
-const response = await fetch(url);
+fetch('http://127.0.0.1:5000/respuesta')
+    .then(response => response.json())
+    .then(data => {console.log(data) 
+            var ph1= document.getElementById('mens1')
+            ph1.innerHTML = `Pedido nro: ${data[0].idRegistros}`
+            var ph2= document.getElementById('mens2')
+            ph2.innerHTML = `Nombre:  ${data[0].nombre}`
+            var ph3= document.getElementById('mens3')
+            ph3.innerHTML = `Apellido:  ${data[0].apellido}`
+            var ph4= document.getElementById('mens4')
+            ph4.innerHTML = `Tema:  ${data[0].tematica}`
+            var ph5= document.getElementById('mens5')
+            ph5.innerHTML = `Orientacion:  ${data[0].orientacion}`
+            var ph6= document.getElementById('mens6')
+            ph6.innerHTML = `Medidas:  ${data[0].medidas}`
+            var ph7= document.getElementById('mens7')
+            ph7.innerHTML = `Precio:  $${data[0].precio}`
+            var ph8= document.getElementById('mens8')
+            ph8.innerHTML = "Su pedido ha sido guardado. Si esta interesado, envíenos la imagen por medio de este correo: pedidos@sicuadra.com. Le responderemos acerca la forma de envío y los medios de pago"
 
-const data = await response.json();
+        })
 
-console.log(data)
+
+    
+    
+    
+    
+
+// Devolucion de los valores
+
+//var ph1 = document.getElementById('mens1');
+//ph1.innerHTML= registro[0] ;
+
+
+
+
+    
+    
 
 
 
